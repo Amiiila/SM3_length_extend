ba5fe52e1a3038368c5e57e402bdf8104e32d01f
Amiiila <110109750+Amiiila@users.noreply.github.com>
2022-07-31 11:05:54 +0800

    Add files via upload

---
+++ SM3_length_extension_attack.py
@@ -0,0 +1,203 @@
+from gmssl import sm3,func
+from time import time
+import random
+import struct
+import binascii
+from math import ceil
+
+IV = [
+    1937774191, 1226093241, 388252375, 3666478592,
+    2842636476, 372324522, 3817729613, 2969243214,
+]
+
+T_j = [
+    2043430169, 2043430169, 2043430169, 2043430169, 2043430169, 2043430169,
+    2043430169, 2043430169, 2043430169, 2043430169, 2043430169, 2043430169,
+    2043430169, 2043430169, 2043430169, 2043430169, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042, 2055708042, 2055708042,
+    2055708042, 2055708042, 2055708042, 2055708042
+]
+list_to_bytes = lambda data: b''.join([bytes((i,)) for i in data])
+bytes_to_list = lambda data: [i for i in data]
+rotl = lambda x, n:((x << n) & 0xffffffff) | ((x >> (32 - n)) & 0xffffffff)
+def sm3_ff_j(x, y, z, j):
+    if 0 <= j and j < 16:
+        ret = x ^ y ^ z
+    elif 16 <= j and j < 64:
+        ret = (x & y) | (x & z) | (y & z)
+    return ret
+
+def sm3_gg_j(x, y, z, j):
+    if 0 <= j and j < 16:
+        ret = x ^ y ^ z
+    elif 16 <= j and j < 64:
+        #ret = (X | Y) & ((2 ** 32 - 1 - X) | Z)
+        ret = (x & y) | ((~ x) & z)
+    return ret
+
+def sm3_p_0(x):
+    return x ^ (rotl(x, 9 % 32)) ^ (rotl(x, 17 % 32))
+
+def sm3_p_1(x):
+    return x ^ (rotl(x, 15 % 32)) ^ (rotl(x, 23 % 32))
+
+def sm3_cf(v_i, b_i):
+    w = []
+    for i in range(16):
+        weight = 0x1000000
+        data = 0
+        for k in range(i*4,(i+1)*4):
+            data = data + b_i[k]*weight
+            weight = int(weight/0x100)
+        w.append(data)
+
+    for j in range(16, 68):
+        w.append(0)
+        w[j] = sm3_p_1(w[j-16] ^ w[j-9] ^ (rotl(w[j-3], 15 % 32))) ^ (rotl(w[j-13], 7 % 32)) ^ w[j-6]
+        str1 = "%08x" % w[j]
+    w_1 = []
+    for j in range(0, 64):
+        w_1.append(0)
+        w_1[j] = w[j] ^ w[j+4]
+        str1 = "%08x" % w_1[j]
+
+    a, b, c, d, e, f, g, h = v_i
+
+    for j in range(0, 64):
+        ss_1 = rotl(
+            ((rotl(a, 12 % 32)) +
+            e +
+            (rotl(T_j[j], j % 32))) & 0xffffffff, 7 % 32
+        )
+        ss_2 = ss_1 ^ (rotl(a, 12 % 32))
+        tt_1 = (sm3_ff_j(a, b, c, j) + d + ss_2 + w_1[j]) & 0xffffffff
+        tt_2 = (sm3_gg_j(e, f, g, j) + h + ss_1 + w[j]) & 0xffffffff
+        d = c
+        c = rotl(b, 9 % 32)
+        b = a
+        a = tt_1
+        h = g
+        g = rotl(f, 19 % 32)
+        f = e
+        e = sm3_p_0(tt_2)
+
+        a, b, c, d, e, f, g, h = map(
+            lambda x:x & 0xFFFFFFFF ,[a, b, c, d, e, f, g, h])
+
+    v_j = [a, b, c, d, e, f, g, h]
+    return [v_j[i] ^ v_i[i] for i in range(8)]
+
+def my_sm3(msg, new_v):
+    # print(msg)
+    len1 = len(msg)
+    reserve1 = len1 % 64
+    msg.append(0x80)
+    reserve1 = reserve1 + 1
+    # 56-64, add 64 byte
+    range_end = 56
+    if reserve1 > range_end:
+        range_end = range_end + 64
+
+    for i in range(reserve1, range_end):
+        msg.append(0x00)
+
+    bit_length = (len1) * 8
+    bit_length_str = [bit_length % 0x100]
+    for i in range(7):
+        bit_length = int(bit_length / 0x100)
+        bit_length_str.append(bit_length % 0x100)
+    for i in range(8):
+        msg.append(bit_length_str[7-i])
+
+    group_count = round(len(msg) / 64) - 1
+
+    B = []
+    for i in range(0, group_count):
+        B.append(msg[(i + 1)*64:(i+2)*64])
+
+    V = []
+    V.append(new_v)
+    for i in range(0, group_count):
+        V.append(sm3_cf(V[i], B[i]))
+
+    y = V[i+1]
+    result = ""
+    for i in y:
+        result = '%s%08x' % (result, i)
+    return result
+
+
+def sm3_kdf(z, klen): # z为16进制表示的比特串（str），klen为密钥长度（单位byte）
+    klen = int(klen)
+    ct = 0x00000001
+    rcnt = ceil(klen/32)
+    zin = [i for i in bytes.fromhex(z.decode('utf8'))]
+    ha = ""
+    for i in range(rcnt):
+        msg = zin  + [i for i in binascii.a2b_hex(('%08x' % ct).encode('utf8'))]
+        ha = ha + sm3_hash(msg)
+        ct += 1
+    return ha[0: klen * 2]
+
+
+def padding(msg):
+    mlen = len(msg)
+    msg.append(0x80)
+    mlen += 1
+    tail = mlen % 64
+    range_end = 56
+    if tail > range_end:
+        range_end = range_end + 64
+    for i in range(tail, range_end):
+        msg.append(0x00)
+    bit_len = (mlen - 1) * 8
+    msg.extend([int(x) for x in struct.pack('>q', bit_len)])
+    for j in range(int((mlen - 1) / 64) * 64 + (mlen - 1) % 64, len(msg)):
+        global pad
+        pad.append(msg[j])
+        global pad_str
+        pad_str += str(hex(msg[j]))
+    return msg
+
+start=time()
+a=str(random.randint(0,2**16))
+m=bytes(a, encoding='utf-8')            #随机生成原始消息
+h=sm3.sm3_hash(bytes_to_list(m))   
+mlen=len(a)
+hlen=len(h)
+extend_m="1234567890"                   #扩展消息
+pad_str = ""
+pad = []
+
+res=[]
+for r in range(0, hlen, 8):
+    res.append(int(h[r:r + 8], 16))     #new_iv
+    
+message=""
+if mlen > 64:
+    for i in range(0, int(mlen / 64) * 64):
+        message += 'a'
+for i in range(0, mlen % 64):
+    message += 'a'
+message = bytes_to_list(bytes(message, encoding='utf-8'))
+message = padding(message)
+message.extend(bytes_to_list(bytes(extend_m, encoding='utf-8')))
+ext_h=my_sm3(message, res)
+print(ext_h)
+
+new_message=""
+new_message = bytes_to_list(m)
+new_message= padding(new_message)
+new_message.extend(bytes_to_list(bytes(extend_m, encoding='utf-8')))
+new_h = sm3.sm3_hash(new_message)
+print(new_h)
+
+if new_h == ext_h:
+    print("长度攻击成功")
+
